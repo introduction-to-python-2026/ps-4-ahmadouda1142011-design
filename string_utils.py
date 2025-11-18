391dd82cd74e24c5d828b44d98c8c4fed3263b73
ahmadouda1142011-design <ahmadouda1142011@gmail.com>
2025-11-18 23:41:32 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,13 @@
 def split_before_each_uppercases(formula):
-    parts = []
-    start = 0
+    parts = []     
+    start = 0       
+
     for i in range(1, len(formula)):
-        if formula[i].isupper():
-            parts.append(formula[start:i])
-            start = i
-    parts.append(formula[start:])
+        if formula[i].isupper():     
+            parts.append(formula[start:i])  
+            start = i                
+
+    parts.append(formula[start:])    
     return parts
 
 
@@ -16,8 +18,13 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
+    
     for i, ch in enumerate(formula):
-        if ch.isdigit():
-            return formula[:i], int(formula[i:])
+        if ch.isdigit():           
+            prefix = formula[:i]
+            number = int(formula[i:])  
+            return prefix, number
+
+    
     return formula, 1
 
